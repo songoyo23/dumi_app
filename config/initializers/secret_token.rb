bc63c961373b6995fefd087923d7a1385d0bc911
Miguel Obando <songoyo@gmail.com>
2013-07-23 22:23:35 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DumiApp::Application.config.secret_key_base = 'fc73d9a90939a5e7328eb6f3fec1c0ed3e0aeba4a4a9aebc18998f1d4c66ee2cfc559e2378d43896b4819ff5b107a901d4d981acb986b8dd4b024e0e22f81a59'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DumiApp::Application.config.session_store :cookie_store, key: '_dumi_app_session'
